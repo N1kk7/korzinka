f21bab8e081a179d57ef37ab66720053a71238a9
Nikk7 <n.sliyusar@gmail.com>
2025-02-23 23:43:20 +0200

    update components

--- stores/lang-store.ts
+++ stores/lang-store.ts
@@ -1,26 +1,6 @@
 // stores/language.ts
 import { defineStore } from 'pinia';
 
-// export const useLanguageStore = defineStore('language', {
-//   state: () => ({
-//     currentLanguage: 'uk', // Язык по умолчанию
-//   }),
-//   actions: {
-//     setLanguage(lang: string) {
-//       this.currentLanguage = lang;
-//       localStorage.setItem('language', lang);
-//     },
-//     loadLanguage() {
-//       const savedLanguage = localStorage.getItem('language');
-//       if (savedLanguage) {
-//         this.currentLanguage = savedLanguage;
-//       }
-//     },
-//   },
-// });
-
-// import { defineStore } from 'pinia';
-
 export const useLanguageStore = defineStore('language', {
   state: () => ({
     currentLanguage: 'uk', // Язык по умолчанию
