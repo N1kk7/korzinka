4154eb5661ba8cf98b55f0f39d2e588fccdabe64
Nikk7 <n.sliyusar@gmail.com>
2025-03-10 19:53:22 +0200

    add createUser

--- server/services/authServices.ts
+++ server/services/authServices.ts
@@ -5,7 +5,7 @@ import restoreUser from "./methods/auth/restoreUser";
 import loginUser from "./methods/auth/loginUser";
 import logoutUser from "./methods/auth/logoutUser";
 import refreshToken from "./methods/auth/refreshToken";
-
+import createUser from "./methods/auth/createUser";
 import me from "./methods/auth/me";
 
 
@@ -16,5 +16,6 @@ export {
     loginUser,
     logoutUser,
     refreshToken,
+    createUser,
     me,
 }
