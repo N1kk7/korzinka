ebf69da96167543624c82d78e4568751d61094db
Nikk7 <n.sliyusar@gmail.com>
2025-03-10 19:51:32 +0200

    remove createUser

--- server/services/authServices.ts
+++ server/services/authServices.ts
@@ -1,15 +1,15 @@
-import createUser from "./methods/auth/createUser";
+
 import deleteUser from "./methods/auth/deleteUser";
 import updateUser from "./methods/auth/updateUser";
 import restoreUser from "./methods/auth/restoreUser";
 import loginUser from "./methods/auth/loginUser";
 import logoutUser from "./methods/auth/logoutUser";
 import refreshToken from "./methods/auth/refreshToken";
+
 import me from "./methods/auth/me";
 
 
 export {
-    createUser,
     deleteUser,
     updateUser,
     restoreUser,
