70fb1f15326d200036e786ad77fdbab8fc7f4f3f
Nikk7 <n.sliyusar@gmail.com>
2025-03-10 20:06:02 +0200

    update file

--- server/services/authServices.ts
+++ server/services/authServices.ts
@@ -5,7 +5,7 @@ import restoreUser from "./methods/auth/restoreUser";
 import loginUser from "./methods/auth/loginUser";
 import logoutUser from "./methods/auth/logoutUser";
 import refreshToken from "./methods/auth/refreshToken";
-import createUser from "./methods/auth/createUser";
+import createUser from "./methods/auth/createUser_tmp";
 import me from "./methods/auth/me";
 
 
